1305853f3340bd55c526c7f24ea81bb6ba08b847
abdallahna <abdnatsheh33@gmail.com>
2022-11-04 19:29:11 +0200

    second commit of empty project

---
+++ src/main/java/Tools/ReadCsvFile.java
@@ -0,0 +1,28 @@
+package Tools;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadCsvFile {
+
+	public static List<String[]> readAllLines(String filePath) throws Exception {
+		CSVReader reader = new CSVReader(new FileReader(filePath));
+
+		List<String[]> data = new ArrayList<String[]>();
+
+		// read line by line
+		String[] record = null;
+
+		while ((record = reader.readNext()) != null) {
+			data.add(record);
+			
+		}		
+		reader.close();
+		return data;
+	}
+	
+
+}
